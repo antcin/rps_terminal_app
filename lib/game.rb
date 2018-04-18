dd172fabc0a4bc658ef22acbd4ea0b6804ba26f9
antcin <ciniglioantonio@gmail.com>
2018-04-18 09:56:50 +0100

    test and implement outcome method; test for checking input, RuntimeError

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,7 @@
 class Game
 
+  WEAPONS = ['r', 'p', 's']
+
   def initialize(std_out = $stdout, std_in = $stdin)
     @std_out = std_out
     @std_in = std_in
@@ -12,5 +14,24 @@ class Game
   def select
     @std_in.read
   end
+
+  def check_input(select)
+    raise 'Not a valid choice' unless WEAPONS.include?(select)
+  end
+
+  def computer_choice
+    WEAPONS.sample
+  end
+
+  def outcome(player_choice, computer_choice)
+   return 'Computer wins' if computer_choice == 's' && player_choice == 'p'
+   return 'Computer wins' if computer_choice == 'paper' && player_choice == 'rock'
+   return 'Computer wins' if computer_choice == 'rock' && player_choice == 'scissors'
+   return 'Draw' if computer_choice == player_choice
+   "Player wins"
+  end
+
+
+
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -22,7 +22,37 @@ describe Game do
       allow(stdin_mock).to receive(:read).and_return "r"
 
       # act
-      expect(subject.select).to eq("r")
+      expect(subject.select).to eq('r')
+    end
+  end
+
+  context "#computer_choice" do
+    it "Computer selects a random weapon" do
+      #arrange
+      srand(0)
+      allow(subject).to receive(:computer_choice).and_return 'r'
+
+      #assert
+      expect(subject.computer_choice).to eq('r')
+      # expect(['r', 'p', 's']).to include(subject.computer_choice)
+    end
+
+    it "Raises an error if input is not 'r', 'p' or 's'" do
+      allow(subject).to receive(:select).and_return 'strawberry'
+
+      expect(subject.select).to raise_error "Not a valid choice"
+    end
+  end
+
+
+  context "#outcome" do
+
+    it "returns draw" do
+      expect(subject.outcome('p', 'p')).to eq 'Draw'
+    end
+
+    it 'returns player wins' do
+      expect(subject.outcome('r', 'p')).to eq 'Player wins'
     end
   end
 
